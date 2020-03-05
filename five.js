17ce3b72e4a78b87c4e03cef1851cb3991e4fac0
aakashDeveloper <ahanda205@gmail.com>
2020-03-05 16:12:02 +0000

    a

---
+++ five.js
@@ -0,0 +1,40 @@
+var city = "lOndoN"
+city.toUpperCase()
+"LONDON"
+var city1 ="HelsInkI"
+city1.toLowerCase()
+"helsinki"
+
+var city2 = "London"
+
+var country = "India"
+
+var country = "India"
+undefined
+country.length
+5
+var country = "  India    "
+undefined
+country.length
+11
+country.trim()
+"India"
+country.trim().length
+5
+
+var country = "India"
+undefined
+country[0]
+"I"
+country[1]
+"n"
+country.slice(1)
+"ndia"
+country.slice(1)
+"ndia"
+country[0].toUpperCase()+country.slice(1).toLowerCase()
+"India"
+var country= "UnItedStAte"
+undefined
+country[0].toUpperCase()+country.slice(1).toLowerCase()
+"Unitedstate"
---
+++ fourth.js
@@ -0,0 +1,71 @@
+console.log('sfg')
+//To debug application
+
+alert("form Submitted")
+//For popup message
+
+confirm('Do you want to leave')
+confirm('Do you want to leave')
+true
+confirm('Do you want to leave')
+false
+var a = confirm('Do you want to leave')
+undefined
+a
+true
+var a = confirm('Do you want to leave')
+undefined
+a
+false
+
+//To take input
+prompt('What is your age')
+prompt('What is your age')
+"22"
+var a = prompt('What is your age')
+undefined
+a
+"22"
+
+var first = prompt('Enter first value')
+var second = prompt('Enter second value')
+var sum = first+second
+alert("Sum is "+sum)
+
+var first = prompt('Enter first value')
+var second = prompt('Enter second value')
+var sum = first+second
+alert("Sum is "+sum)
+undefined
+first
+"10"
+second
+"10"
+first+second
+"1010"
+parseInt(first)+parseInt(second)
+20
+parseInt(first)
+10
+parseInt('first')
+NaN
+parseInt('10')
+10
+parseInt('abc')
+NaN
+
+parseInt('10.20')
+
+parseInt('10.20')
+10
+parseFloat('10.20')
+10.2
+
+var a = "10"
+var b = 5
+typeof(b)
+"number"
+b.toString()
+"5"
+
+b-a = 5-"10" =   "510"
