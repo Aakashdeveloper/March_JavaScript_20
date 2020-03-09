ccf68551ba26512a00b848b66d87de261ea68df6
aakashDeveloper <ahanda205@gmail.com>
2020-03-09 16:21:48 +0000

    a

---
+++ eight.js
@@ -0,0 +1,104 @@
+var  a = '10'
+
+if(a){
+    do something
+}else{
+    do something
+}
+/////
+
+if(logic){
+    do something
+}else{
+    do something
+}
+
+
+var a = 12
+if(a%2==0){
+    console.log('Number is even')
+}else if(a%3==0){
+    console.log('Number is divisible by 3')
+}
+else{
+    console.log('Number is odd')
+}
+
+var a = 12
+if(a%2==0){
+    console.log('Number is even')
+}else if(a%3==0){
+    console.log('Number is divisible by 3')
+}
+else{
+    console.log('Number is odd')
+}
+
+
+var a = 25
+if(a%2==0){
+    console.log('Number is even')
+}if(a%3==0 || a%5==0){
+    console.log('Number is divisible by 3 or 5')
+}
+else{
+    console.log('Number is odd')
+}
+
+
+var a = 9
+if(a%2==0){
+    console.log('Number is even')
+}if(a%3==0||a%5==0){
+    console.log('i am in this block')
+    if(a%3==0){
+        console.log('Number is divisible by 3')
+    }
+    if(a%5==0){
+        console.log('Number is divisible by 5')
+    }
+    
+}
+else{
+    console.log('Number is odd')
+}
+
+
+
+Admin 
+superadmin
+
+
+
+var a = 10
+
+a==10?"hii":"bie"
+
+var a = 10
+
+a==10?"hii":"bie"
+"hii"
+var a = 10
+
+a==11?"hii":"bie"
+"bie"
+var a = 10
+
+a==10?a+1:a-1
+11
+var a = 11
+
+a==10?a+1:a-1
+10
+
+////////////
+Luck Game
+> Generate one number between 1 to 100
+> take one input from user also 
+> if both match than say win else loose
+
+
+
+
+
+
---
+++ seven.js
@@ -0,0 +1,124 @@
+var a = 10
+Math.hypot(10,11,122)
+122.90240030202828
+var a = [10,1,2]
+undefined
+Math.hypot(...a)
+10.2469507659596
+var a = 10
+undefined
+a>12
+false
+a<12
+true
+a==10
+true
+a===10
+true
+a !=10
+false
+a !==10
+false
+
+-------
+var a = 10
+undefined
+a =!= 10
+VM968:1 Uncaught SyntaxError: Unexpected token '!='
+a != 10
+false
+var x = true
+undefined
+var y =  !x
+undefined
+y
+false
+!y
+true
+!a
+false
+var a = "hiii"
+undefined
+!a
+false
+var a = 100
+undefined
+!a
+false
+var a = false
+undefined
+!a
+truevar a = 10
+undefined
+
+a != 10
+false
+var x = true
+undefined
+var y =  !x
+undefined
+y
+false
+!y
+true
+!a
+false
+var a = "hiii"
+undefined
+!a
+false
+var a = 100
+undefined
+!a
+false
+var a = false
+undefined
+!a
+true
+
+1&&1
+1
+1&&0
+0
+0&&1
+0
+0&&0
+0
+true&&true
+true
+
+var a = 10
+undefined
+var b = "hii"
+undefined
+a&&b
+"hii"
+b&&a
+10
+var c = true
+undefined
+c&&a
+10
+a&&c
+true
+true || true
+true
+true || false
+true
+0 || 0
+0
+a||b
+10
+a&&b
+"hii"
+
+var a = ''
+undefined
+a == true
+false
+var a = '10'
+undefined
+a == true
+false
+a === true
+false
