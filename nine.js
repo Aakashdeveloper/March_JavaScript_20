1fe75184dea10403ba5b6cd9dde2e7088ac459c9
aakashDeveloper <ahanda205@gmail.com>
2020-03-10 16:25:00 +0000

    a

---
+++ nine.js
@@ -0,0 +1,70 @@
+Date()
+"Tue Mar 10 2020 15:57:30 GMT+0000 (Greenwich Mean Time)"
+var a = new Date()
+undefined
+a
+Tue Mar 10 2020 16:05:26 GMT+0000 (Greenwich Mean Time)
+var a = new Date().getDate()
+undefined
+a
+10
+var a = new Date().getDay()
+undefined
+a
+2
+var a = new Date().getYear()
+undefined
+a
+120
+var a = new Date().getTime()
+undefined
+a
+1583856413628
+
+
+var a = new Date().getDay()
+undefined
+a
+2
+
+///
+switch(condition){
+    case '':
+        output
+    case'':
+        output
+    default:
+        output
+}
+
+
+switch(new Date().getDay()){
+    case 0:
+        console.log('Today is sunday')
+        break;
+    case 1:
+        console.log('Today is Monday')
+        break;
+    case 2:
+        console.log('Today is Tuesday')
+        break;
+    default:
+        console.log('not Valid date')
+
+}
+
+
+var name = 'John'
+switch(name){
+    case 'aakash':
+        console.log('You are user')
+        break;
+    case 'John':
+        console.log('You are Admin')
+        break;
+    case 'Eva':
+        console.log('You are SysAdmin')
+        break;
+    default:
+        console.log('I Dont know you')
+}
---
+++ ten.js
@@ -0,0 +1,17 @@
+localStorage.getItem('token')
+"ejbf7hjc87hbdc"
+localStorage.removeItem('token')
+undefined
+localStorage.setItem('token','ejbf7hjc87hbdc')
+undefined
+localStorage.removeItem('token')
+undefined
+localStorage.getItem('token')
+null
+
+//
+Same luck Game
+> You need to allow user total 5 changes
+in one game and switch casee keep telling
+user how many change left
+after 5 chance say game over
