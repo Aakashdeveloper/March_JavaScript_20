e889f2f9be2f374845f8952e8475229c900636c3
aakashDeveloper <ahanda205@gmail.com>
2020-03-26 16:25:15 +0000

    a

--- currencyyconvert/view.js
+++ currencyyconvert/view.js
@@ -1,11 +1,33 @@
 var url = "https://api.exchangeratesapi.io/latest?base="
 
-function convert(){
+
+async function convert(){
     var base = document.getElementById('base').value;
-    var output = document.getElementById('output').value;
+    var convert = document.getElementById('output').value;
+    var amount = document.getElementById('amount').value;
+    let response =  await fetch(`${url}${base}`, {method:'GET'})
+    let data = await response.json();
+    final = data.rates
+    final = `${convert} ${final[convert]*parseFloat(amount)}`
+    console.log(final)
+    document.getElementById('converted').innerText=final
+}
+
+
+/*function convert(){
+    var base = document.getElementById('base').value;
+    var convert = document.getElementById('output').value;
+    var amount = document.getElementById('amount').value;
+    var final;
+    //alert(base)
     console.log(`${url}${base}`)
     fetch(`${url}${base}`, {method:'GET'})
     .then((response) => response.json())
-    .then((out) =>  {console.log(JSON.stringify(out.rates))})
-
+    .then((out) =>  {
+        final = out.rates
+        final = `${convert} ${final[convert]*parseFloat(amount)}`
+        console.log(final)
+        document.getElementById('converted').innerText=final
+    })
 }
+*/
